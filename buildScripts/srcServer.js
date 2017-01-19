70ceae2021a4814e999c315280b3e0006d8c0f04
Carlos Alberto Villamizar Morantes <calvimor@gmail.com>
2017-01-19 15:52:15 -0500

    Configured Webpack with Express

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 const port = 3000;
 const app = express();
