01ca5570eaf0368f14690477f9e4ad7f31646cf0
Carlos Alberto Villamizar Morantes <calvimor@gmail.com>
2017-01-24 08:04:11 -0500

    ESLint set up

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,5 @@
 //Example of using ES6 and transpiling with babel
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode'));
+console.log(chalk.green('Starting app in dev mode')); // eslint-disable-line no-console
+console.log(chalk.bgYellow('Omitted a console message before this')); // when execute npm run lint
