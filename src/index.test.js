b3870a358ab2d6a4418b6e69b642a6699482cfa8
Carlos Alberto Villamizar Morantes <calvimor@gmail.com>
2017-01-24 09:18:59 -0500

    DOM testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,21 @@
 import { expect } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs'; //filesystem
 
 describe('Our first test', () => {
   it('should pass', () => {
    expect(true).to.equal(true);
+  });
+});
+
+describe('jsdom test', () => {
+  it('should pass', (done) => {
+    const indexHtml = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(indexHtml, function (err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello Calvimor!");
+      done();
+      window.close();
+    });
   });
 });
